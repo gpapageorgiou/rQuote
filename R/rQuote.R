ad8abac26b43a6b732f1cb90c06a6c85491a5a1c
gpapageorgiou <papageorgiou.grigorios@gmail.com>
2019-07-22 21:34:16 +0200

    DESCRIPTION Imports and base function

---
+++ R/rQuote.R
@@ -0,0 +1,40 @@
+rQuote <- function(tag = 'science', page_range = 10,
+                            cores = detectCores() - 1) {
+
+  # check page range
+  if (page_range > 100) {
+    stop('Page range should be equal or less than 100')
+  }
+
+  if (page_range < 100) {
+    pages <- sample(1:100, page_range, replace = FALSE)
+  } else {
+    pages <- 1:page_range
+  }
+
+  # create initial url according to tag input
+  url <- paste0('https://www.goodreads.com/quotes/tag/', tag)
+
+  # create list of urls
+  urls <- lapply(pages, FUN = function (x, url) paste0(url, '?page=', x), url)
+  urls <- do.call(c, urls)
+
+  #
+  cl <- makeCluster(cores)
+  registerDoParallel(cl)
+
+  res <- foreach(i = seq_along(urls),
+                 .packages = c("rvest", 'xml2'),
+                 .combine = rbind) %dopar% {
+                  quote <- html_session(urls[i]) %>% html_nodes('div.quoteText') %>% html_text()
+                  author <- html_session(urls[i]) %>% html_nodes('span.authorOrTitle') %>% html_text()
+                  #quote <- gsub('(.*)[ ]["](.*)["][\n](.*)', "\\2", quote)
+                  quote <- gsub('(.*)[ ][\u201c](.*)[\u201d][\n](.*)', "\\2", quote)
+                  author <- gsub('([,\n|\n]*)([^,])', '\\2', author)
+                  #author <- gsub('(.*)[ ]([:upper:].*[:lower:])[,?\n *]', '\\1', author)
+                  cbind(quote, author)
+                 }
+  stopCluster(cl)
+
+  res[sample(nrow(res), 1), ]
+}
