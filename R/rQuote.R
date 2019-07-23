ce0457bef773ee3884fb0e8b6294eed1a96362ad
gpapageorgiou <papageorgiou.grigorios@gmail.com>
2019-07-23 11:47:08 +0200

    Added plot output

--- R/rQuote.R
+++ R/rQuote.R
@@ -41,15 +41,41 @@ rQuote <- function(tag = 'science', page_range = 10,
   random_row <- sample(nrow(res), 1)
   quote <- res[random_row, 'quote']
   author <- trimws(res[random_row, 'author'])
+  author <- paste0("-  ", author)
 
-  cat(quote, "\n", "\n", '-', author)
+  linebreaks <- strrep('\n', 4)
+  whitespace <- strrep(' ', 100)
+
+  font_add_google('Francois One', 'Francois One')
+
+  showtext_auto()
+
+  cat(quote, "\n", "\n", author)
+
+  theme <- match.arg(theme)
+  if (theme == 'light') {
+    outplot <- ggplot() + xlim(0, 4) + ylim(0, 4) + theme_void() +
+      theme(panel.background = element_rect(fill = '#f5f5f5')) +
+      geom_text(aes(x = 2, y = 2), label = paste("“", wrapit(quote), "”",
+                                                 linebreaks,
+                                                 whitespace, author),
+                size = 6, color = "#363636",
+                family = 'Francois One')
+  } else {
+    outplot <- ggplot() + xlim(0, 4) + ylim(0, 4) + theme_void() +
+      theme(panel.background = element_rect(fill = '#363636')) +
+      geom_text(aes(x = 2, y = 2), label = paste("“", wrapit(quote), "”",
+                                                 linebreaks,
+                                                 whitespace, author),
+                size = 6, color = "#f5f5f5" ,
+                family = 'Francois One')
+  }
 
   OS <- match.arg(OS)
   switch(OS,
-         windows = win.graph(),
+         windows = win.graph(width = 18, height = 9),
          linux = x11(),
          macOS = quartz())
 
-
-
+  print(outplot)
 }
