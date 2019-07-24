2267eab162dfb11eee152b698fe8c77cdf0ee3f2
gpapageorgiou <papageorgiou.grigorios@gmail.com>
2019-07-24 16:58:07 +0200

    Added popup argument
    Added hex logo quotes in popup

--- R/rQuote.R
+++ R/rQuote.R
@@ -8,7 +8,8 @@
 rQuote <- function(tag = 'science', page_range = 1,
                             cores = detectCores() - 1,
                    OS = c('windows', 'linux', 'macOS'),
-                   theme = c('light', 'dark')) {
+                   theme = c('light', 'dark'),
+                   popup = TRUE) {
 
   # check page range
   if (page_range > 100) {
@@ -56,39 +57,58 @@ rQuote <- function(tag = 'science', page_range = 1,
     stop("No quote found. Probably the tag you are searching for does not exist. Change tag and try again.")
   }
 
-  linebreaks <- strrep('\n', 4)
-  whitespace <- strrep(' ', 180)
+  if (popup) {
+    linebreaks <- strrep('\n', 4)
+    whitespace <- strrep(' ', 180)
 
-  font_add_google('Francois One', 'Francois One')
+    font_add_google('Francois One', 'Francois One')
+    font_add_google('Fredericka the Great', 'Fredericka the Great')
 
-  showtext_auto()
+    showtext_auto()
 
-  cat(quote, "\n", "\n", author)
+    theme <- match.arg(theme)
+    if (theme == 'light') {
+      outplot <- ggplot() + xlim(0, 4) + ylim(0, 4) + theme_void() +
+        theme(panel.background = element_rect(fill = '#f5f5f5')) +
+        geom_text(aes(x = 2, y = 2), label = paste("\u201c", wrapit(quote), "\u201d",
+                                                   linebreaks,
+                                                   whitespace, author),
+                  size = 7, color = "#363636",
+                  family = 'Francois One') +
+        geom_text(aes(x = 3.75, y = 0), label = paste("\u201c"),
+                  size = 50, color = '#363636',
+                  family = 'Fredericka the Great') +
+        geom_text(aes(x = 4, y = 0), label = paste("\u201d"),
+                  size = 50, color = '#363636',
+                  family = 'Fredericka the Great')
+    } else {
+      outplot <- ggplot() + xlim(0, 4) + ylim(0, 4) + theme_void() +
+        theme(panel.background = element_rect(fill = '#363636')) +
+        geom_text(aes(x = 2, y = 2), label = paste("\u201c", wrapit(quote), "\u201d",
+                                                   linebreaks,
+                                                   whitespace, author),
+                  size = 7, color = "#f5f5f5" ,
+                  family = 'Francois One') +
+        geom_text(aes(x = 3.75, y = 0), label = paste("\u201c"),
+                  size = 50, color = '#f5f5f5',
+                  family = 'Fredericka the Great') +
+        geom_text(aes(x = 4, y = 0), label = paste("\u201d"),
+                  size = 50, color = '#f5f5f5',
+                  family = 'Fredericka the Great')
+    }
 
-  theme <- match.arg(theme)
-  if (theme == 'light') {
-    outplot <- ggplot() + xlim(0, 4) + ylim(0, 4) + theme_void() +
-      theme(panel.background = element_rect(fill = '#f5f5f5')) +
-      geom_text(aes(x = 2, y = 2), label = paste("\u201c", wrapit(quote), "\u201d",
-                                                 linebreaks,
-                                                 whitespace, author),
-                size = 7, color = "#363636",
-                family = 'Francois One')
+    OS <- match.arg(OS)
+    switch(OS,
+           windows = win.graph(width = 32, height = 16),
+           linux = x11(),
+           macOS = quartz())
+
+    print(outplot)
+
+    cat(quote, "\n", "\n", author)
   } else {
-    outplot <- ggplot() + xlim(0, 4) + ylim(0, 4) + theme_void() +
-      theme(panel.background = element_rect(fill = '#363636')) +
-      geom_text(aes(x = 2, y = 2), label = paste("\u201c", wrapit(quote), "\u201d",
-                                                 linebreaks,
-                                                 whitespace, author),
-                size = 7, color = "#f5f5f5" ,
-                family = 'Francois One')
+    cat(quote, "\n", "\n", author)
   }
 
-  OS <- match.arg(OS)
-  switch(OS,
-         windows = win.graph(width = 32, height = 16),
-         linux = x11(),
-         macOS = quartz())
 
-  print(outplot)
 }
